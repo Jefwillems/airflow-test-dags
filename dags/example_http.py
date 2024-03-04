a04224a2174d092c68ff85b361f1339f64bd9d6b
Jefwillems <jefwillems100@gmail.com>
2024-03-04 16:52:30 +0100

    feat: task tryout

--- dags/example_http.py
+++ dags/example_http.py
@@ -3,23 +3,29 @@ import json
 
 from airflow import DAG
 from airflow.providers.http.operators.http import HttpOperator
+from airflow.decorators import task, dag
 
 
-with DAG(
+@dag(
     dag_id="example_http_operator",
     default_args={"retries": 1},
     tags=["example"],
     start_date=datetime(2021, 1, 1),
     catchup=False,
-) as dag:
+    doc_md=__doc__,
+)
+def example_http_operator():
+    @task()
+    def get_time():
+        return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-    dag.doc_md = __doc__
+    t = get_time()
 
     task_post_op = HttpOperator(
         http_conn_id="httpbin_conn",
         task_id="simple_http_post",
         endpoint="post",
-        data=json.dumps({"data": "data"}),
+        data=json.dumps({"data": t}),
         headers={"Content-Type": "application/json"},
     )
 
